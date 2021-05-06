b0c30d7a3313b9c7616115e48a5e4fe89438d0b4
Navneet Melarkode <navneet.melarkode@gmail.com>
2021-05-06 16:59:40 +0530

    feat: backend logic complete

---
+++ index.js
@@ -0,0 +1,86 @@
+const argv = require("yargs").argv;
+const Pixel = require("./pixel-stuff");
+const image = argv.image;
+const write = argv.write;
+const number = argv.number;
+const type = argv.type;
+const option = argv.option;
+
+console.time("start");
+if (!image) {
+	console.error("You must provide an image with the --image flag");
+	process.exit(1);
+}
+
+if (!number && type === "kmeans") {
+	console.error(
+		"You must enter number of colours required in the palette with the --number flag"
+	);
+	process.exit(1);
+}
+
+if (!option && type === "mediancut") {
+	console.error(
+		"You must provide the sorting criterion with the --option flag"
+	);
+	process.exit(1);
+}
+
+if (!type) {
+	console.error("You must provide the method with the --type flag");
+	process.exit(1);
+}
+Pixel.load_pixels(image).then((pixels) => {
+	if (type === "kmeans") {
+		var centers = Pixel.fit(pixels, number);
+		col = [];
+		centers.forEach((v) => {
+			col.push(v.center);
+		});
+		col = Pixel.orderByLuminance(col);
+	} else if (type === "mediancut") {
+		var bucket = Pixel.quantize(pixels);
+		var col =
+			option === "hue"
+				? Pixel.orderByHue(bucket)
+				: Pixel.orderByLuminance(bucket);
+	}
+	const fs = require("fs");
+	paletteHTML = `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8"/>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<title>Palette for ${image}</title>
+			<style>
+				html,body{
+					width: 100%;
+					height:100%;
+					margin: 0;
+					padding: 0;
+				}
+				body{
+					display: flex;
+					flex-wrap: wrap;
+				}
+				.color{
+					width: 25%;
+					height: 25%;
+				}
+			</style>
+		</head>
+		<body>
+			${col.reduce((prev, color) => {
+				return (
+					prev +
+					`<div class="color" style="background-color: rgb(${color.r},${color.g},${color.b})"></div>`
+				);
+			}, "")}
+		</body>
+		</html>
+	`;
+	fs.writeFileSync("palette.html", paletteHTML);
+	console.timeEnd("start");
+});
---
+++ pixel-stuff.js
@@ -0,0 +1,244 @@
+const getPixels = require("get-pixels");
+const _ = require("lodash");
+
+class Pixel {
+	static get DEFAULT_DEPTH() {
+		return 4;
+	}
+	static get MIN_DIFF() {
+		return 5;
+	}
+	static load_pixels(image) {
+		return new Promise((resolve, reject) => {
+			getPixels(image, (err, pixels) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(Pixel._convertPixelsToRGB(pixels));
+			});
+		});
+	}
+
+	static _convertPixelsToRGB(pixels) {
+		const width = pixels.shape[0];
+		const height = pixels.shape[1];
+		var rgb = [];
+		for (var i = 0; i < height; i++) {
+			for (var j = 0; j < width; j++) {
+				var index = (j + i * width) * 4;
+				rgb.push({
+					r: pixels.data[index],
+					g: pixels.data[index + 1],
+					b: pixels.data[index + 2],
+				});
+			}
+		}
+		return rgb;
+	}
+
+	static findBiggestRange(rgb) {
+		let rMin = Number.POSITIVE_INFINITY;
+		let rMax = Number.NEGATIVE_INFINITY;
+
+		let gMin = Number.POSITIVE_INFINITY;
+		let gMax = Number.NEGATIVE_INFINITY;
+
+		let bMin = Number.POSITIVE_INFINITY;
+		let bMax = Number.NEGATIVE_INFINITY;
+
+		rgb.forEach((pixel) => {
+			rMin = Math.min(rMin, pixel.r);
+			rMax = Math.max(rMax, pixel.r);
+			gMin = Math.min(gMin, pixel.g);
+			gMax = Math.max(gMax, pixel.g);
+			bMin = Math.min(bMin, pixel.b);
+			bMax = Math.max(bMax, pixel.b);
+		});
+
+		const rRange = rMax - rMin;
+		const gRange = gMax - gMin;
+		const bRange = bMax - bMin;
+
+		const biggestRange = Math.max(rRange, gRange, bRange);
+		if (biggestRange === rRange) {
+			return "r";
+		}
+		if (biggestRange === gRange) {
+			return "g";
+		}
+		if (biggestRange === bRange) {
+			return "b";
+		}
+	}
+
+	static quantize(rgb, depth = 0, maxDepth = Pixel.DEFAULT_DEPTH) {
+		if (depth === 0) {
+			console.log(`Quantizing to ${Math.pow(2, maxDepth)} buckets.`);
+		}
+		if (depth === maxDepth) {
+			const color = rgb.reduce(
+				(prev, curr) => {
+					prev.r += curr.r;
+					prev.g += curr.g;
+					prev.b += curr.b;
+					return prev;
+				},
+				{
+					r: 0,
+					g: 0,
+					b: 0,
+				}
+			);
+			color.r = Math.round(color.r / rgb.length);
+			color.g = Math.round(color.g / rgb.length);
+			color.b = Math.round(color.b / rgb.length);
+
+			return [color];
+		}
+		const filterToSort = Pixel.findBiggestRange(rgb);
+		rgb.sort((p1, p2) => {
+			return p1[filterToSort] - p2[filterToSort];
+		});
+
+		const mid = rgb.length / 2;
+		return [
+			...Pixel.quantize(rgb.slice(0, mid), depth + 1, maxDepth),
+			...Pixel.quantize(rgb.slice(mid + 1), depth + 1, maxDepth),
+		];
+	}
+
+	static orderByLuminance(rgb) {
+		const calcLuminance = (p) => {
+			return 0.2126 * p.r + 0.7152 * p.g + 0.0722 * p.b;
+		};
+
+		return rgb.sort((p1, p2) => {
+			return -calcLuminance(p1) + calcLuminance(p2);
+		});
+	}
+
+	static orderByHue(rgb) {
+		const calcHue = (p) => {
+			var h;
+			var R, G, B, min, max;
+			R = p.r / 255;
+			G = p.g / 255;
+			B = p.b / 255;
+			min = Math.min(R, G, B);
+			max = Math.max(R, G, B);
+			if (max === R) {
+				h = (G - B) / (max - min);
+			} else if (max === G) {
+				h = 2 + (B - R) / (max - min);
+			} else if (max === B) {
+				h = 4 + (R - G) / (max - min);
+			}
+			h *= 60;
+			if (h > 0) {
+				return Math.floor(h);
+			} else {
+				return Math.floor(360 - h);
+			}
+		};
+
+		return rgb.sort((p1, p2) => {
+			return -calcHue(p1) + calcHue(p2);
+		});
+	}
+
+	static calcMostVariance(rgb) {
+		var index = 0;
+		var max = Number.NEGATIVE_INFINITY;
+		rgb
+			.map((p) => Math.max(p.r, p.g, p.b) - Math.min(p.r, p.g, p.b))
+			.forEach((v, i) => {
+				if (v > max) {
+					index = i;
+					max = v;
+				}
+			});
+		return rgb[index];
+	}
+
+	static euclid(p1, p2) {
+		return Math.sqrt(
+			Math.pow(p1.r - p2.r, 2) +
+				Math.pow(p1.g - p2.g, 2) +
+				Math.pow(p1.b - p2.b, 2)
+		);
+	}
+
+	static findCenter(rgb) {
+		const color = rgb.reduce(
+			(prev, curr) => {
+				prev.r += curr.r;
+				prev.g += curr.g;
+				prev.b += curr.b;
+				return prev;
+			},
+			{
+				r: 0,
+				g: 0,
+				b: 0,
+			}
+		);
+		color.r = Math.round(color.r / rgb.length);
+		color.g = Math.round(color.g / rgb.length);
+		color.b = Math.round(color.b / rgb.length);
+		return color;
+	}
+
+	static assignPoints(cluster, rgb) {
+		var fin = {};
+		cluster.forEach((v, i) => {
+			fin[i] = [];
+		});
+		rgb.forEach((p) => {
+			var index = 0;
+			var min = Number.POSITIVE_INFINITY;
+			cluster.forEach((v, i) => {
+				var dis = Pixel.euclid(v.center, p);
+				if (dis < min) {
+					min = dis;
+					index = i;
+				}
+			});
+			fin[index].push(p);
+		});
+		return fin;
+	}
+
+	static fit(rgb, n) {
+		var c = [];
+		_.sampleSize(rgb, n).forEach((v) => {
+			c.push({
+				center: v,
+				points: [],
+			});
+		});
+		var nm = 0;
+		while (nm < 15) {
+			var final = Pixel.assignPoints(c, rgb);
+			var diff = Number.NEGATIVE_INFINITY;
+			for (var i = 0; i < n; i++) {
+				if (!final[i]) {
+					continue;
+				}
+				var old = c[i];
+				var newCenter = Pixel.findCenter(final[i]);
+				c[i] = {
+					center: newCenter,
+					points: final[i],
+				};
+				diff = Math.max(diff, Pixel.euclid(old.center, newCenter));
+			}
+			if (diff < Pixel.MIN_DIFF) {
+				console.log("Breaking bad");
+				break;
+			}
+			nm += 1;
+		}
+		return c;
+	}
+}
+module.exports = Pixel;
